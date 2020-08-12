5c8bf4257c721204219149f080855706770fe0af
AOSOUL <63068659+AOSOUL@users.noreply.github.com>
2020-08-11 21:19:17 -0700

    Add files via upload

---
+++ js/effect.js
@@ -0,0 +1,11 @@
+// var url="https://tcp.kirincloud.net/";
+// function currency(){
+//     $("#top").load("../publicPages/top.html");
+//     $("#footer").load("../publicPages/foot.html");
+//     $("#qq").load("../publicPages/qq.html");
+//     $("#right_con").load("../publicPages/rightCon.html");
+//     $("#btn_alink").load("../publicPages/btn_tdun.html");
+//     $("#freebox").load("../publicPages/freebox.html");
+   
+// }
+// currency();
